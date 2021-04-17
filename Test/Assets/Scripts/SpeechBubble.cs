483e50713afa6b24ddca17c34f5195f103799607
CaptainJellie <37323485+CaptainJellie@users.noreply.github.com>
2021-04-17 15:13:54 -0700

    hi
    pls

---
+++ Test/Assets/Scripts/SpeechBubble.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechBubble : MonoBehaviour
+{
+    public Animator animator;
+
+    private void Awake()
+    {
+        animator.SetBool("Visible", false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            animator.SetBool("Visible", true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            animator.SetBool("Visible", false);
+        }
+    }
+}
+
---
+++ Test/Assets/Scripts/SpriteCycle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteCycle : MonoBehaviour
+{
+    public Sprite[] sprites;
+    public int currSpriteIndex = 0;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sprites[currSpriteIndex];
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && currSpriteIndex < sprites.Length)
+        {   
+            currSpriteIndex++;
+            spriteRenderer.sprite = sprites[currSpriteIndex];
+        }
+    }
+}
